16fcf5a9cb2f25d988ad2525440193e22a85d850
shweta sharma <shweta29sharma06@gmail.com>
2024-03-01 18:13:33 +0530

    update

--- src/LandingPage/AbodeMainLandingPage.jsx
+++ src/LandingPage/AbodeMainLandingPage.jsx
@@ -4,12 +4,20 @@ import map from "../Assets/map.svg"
 import star from "../Assets/star.svg"
 import { Carousel } from '3d-react-carousal';
 import logoo from "../Assets/logoo.svg"
+import left_grey from "../Assets/left_grey.svg"
+import right_arr from "../Assets/right_arr.svg"
+import left_arr from "../Assets/left_arr.svg"
+import right_grey from "../Assets/right_grey.svg"
 import mainImage from "../Assets/mainImage.svg"
 import mainImage2 from "../Assets/mainImage2.png"
 import mainImage3 from "../Assets/mainImage3.png"
 import img1 from "../Assets/img1.png"
 import img2 from "../Assets/img2.png"
 import img3 from "../Assets/img3.png"
+import changeImage1 from "../Assets/changeImage1.png"
+import changeImage2 from "../Assets/changeImage2.png"
+import changeImage3 from "../Assets/changeImage3.png"
+import txt from "../Assets/txt.png"
 import img4 from "../Assets/img4.png"
 import img5 from "../Assets/img5.png"
 import img6 from "../Assets/img6.png"
@@ -34,10 +42,48 @@ const AbodeMainLandingPage = () => {
         <img style={{
             width: "852px", height: "469px"
         }} src={mainImage3} alt="5" />];
+        const [specification,setSpecification] = useState("livingRoom");
     return (
         <div>
             <div>
-                <div></div>
+                {/* first part */}
+                <div>
+                <div className='first_headdd'>
+                    {/* navbar */}
+                   <div className='top_top'>
+                    <div><img src={logoo} alt='logo'/></div>
+                    <div className='for_gap'>
+                        <span className='first_heading'>OVERVIEW</span>
+                        <span className='first_heading'>AMENITIES</span>
+                        <span className='first_heading'>GALLERY</span>
+                        <span className='first_heading'>FLOOR PLAN</span>
+                        <span className='first_heading'>LOCATION</span>
+                        <span className='first_heading'>CONTACT</span>
+                    </div>
+                    <div className='for_gaaap'>
+                        <span>
+                        <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="20" height="20" viewBox="0 0 48 48">
+<path fill="#fff" d="M4.868,43.303l2.694-9.835C5.9,30.59,5.026,27.324,5.027,23.979C5.032,13.514,13.548,5,24.014,5c5.079,0.002,9.845,1.979,13.43,5.566c3.584,3.588,5.558,8.356,5.556,13.428c-0.004,10.465-8.522,18.98-18.986,18.98c-0.001,0,0,0,0,0h-0.008c-3.177-0.001-6.3-0.798-9.073-2.311L4.868,43.303z"></path><path fill="#fff" d="M4.868,43.803c-0.132,0-0.26-0.052-0.355-0.148c-0.125-0.127-0.174-0.312-0.127-0.483l2.639-9.636c-1.636-2.906-2.499-6.206-2.497-9.556C4.532,13.238,13.273,4.5,24.014,4.5c5.21,0.002,10.105,2.031,13.784,5.713c3.679,3.683,5.704,8.577,5.702,13.781c-0.004,10.741-8.746,19.48-19.486,19.48c-3.189-0.001-6.344-0.788-9.144-2.277l-9.875,2.589C4.953,43.798,4.911,43.803,4.868,43.803z"></path><path fill="#cfd8dc" d="M24.014,5c5.079,0.002,9.845,1.979,13.43,5.566c3.584,3.588,5.558,8.356,5.556,13.428c-0.004,10.465-8.522,18.98-18.986,18.98h-0.008c-3.177-0.001-6.3-0.798-9.073-2.311L4.868,43.303l2.694-9.835C5.9,30.59,5.026,27.324,5.027,23.979C5.032,13.514,13.548,5,24.014,5 M24.014,42.974C24.014,42.974,24.014,42.974,24.014,42.974C24.014,42.974,24.014,42.974,24.014,42.974 M24.014,42.974C24.014,42.974,24.014,42.974,24.014,42.974C24.014,42.974,24.014,42.974,24.014,42.974 M24.014,4C24.014,4,24.014,4,24.014,4C12.998,4,4.032,12.962,4.027,23.979c-0.001,3.367,0.849,6.685,2.461,9.622l-2.585,9.439c-0.094,0.345,0.002,0.713,0.254,0.967c0.19,0.192,0.447,0.297,0.711,0.297c0.085,0,0.17-0.011,0.254-0.033l9.687-2.54c2.828,1.468,5.998,2.243,9.197,2.244c11.024,0,19.99-8.963,19.995-19.98c0.002-5.339-2.075-10.359-5.848-14.135C34.378,6.083,29.357,4.002,24.014,4L24.014,4z"></path><path fill="#40c351" d="M35.176,12.832c-2.98-2.982-6.941-4.625-11.157-4.626c-8.704,0-15.783,7.076-15.787,15.774c-0.001,2.981,0.833,5.883,2.413,8.396l0.376,0.597l-1.595,5.821l5.973-1.566l0.577,0.342c2.422,1.438,5.2,2.198,8.032,2.199h0.006c8.698,0,15.777-7.077,15.78-15.776C39.795,19.778,38.156,15.814,35.176,12.832z"></path><path fill="#fff" fill-rule="evenodd" d="M19.268,16.045c-0.355-0.79-0.729-0.806-1.068-0.82c-0.277-0.012-0.593-0.011-0.909-0.011c-0.316,0-0.83,0.119-1.265,0.594c-0.435,0.475-1.661,1.622-1.661,3.956c0,2.334,1.7,4.59,1.937,4.906c0.237,0.316,3.282,5.259,8.104,7.161c4.007,1.58,4.823,1.266,5.693,1.187c0.87-0.079,2.807-1.147,3.202-2.255c0.395-1.108,0.395-2.057,0.277-2.255c-0.119-0.198-0.435-0.316-0.909-0.554s-2.807-1.385-3.242-1.543c-0.435-0.158-0.751-0.237-1.068,0.238c-0.316,0.474-1.225,1.543-1.502,1.859c-0.277,0.317-0.554,0.357-1.028,0.119c-0.474-0.238-2.002-0.738-3.815-2.354c-1.41-1.257-2.362-2.81-2.639-3.285c-0.277-0.474-0.03-0.731,0.208-0.968c0.213-0.213,0.474-0.554,0.712-0.831c0.237-0.277,0.316-0.475,0.474-0.791c0.158-0.317,0.079-0.594-0.04-0.831C20.612,19.329,19.69,16.983,19.268,16.045z" clip-rule="evenodd"></path>
+</svg>
+                        </span>
+                        <span className='first_heading'>+918790878787</span>
+                    </div>
+                   </div>
+                   <div style={{display: 'flex'}}>
+        <div style={{width: '40%', paddingTop: '3.8%', paddingLeft: '5%'}}>
+          <img src={txt}/>
+          <div style={{paddingTop: '8%', fontSize: '40px', fontWeight: '500',color:"#D6AD60",fontFamily:"Lora"}}>Luxury Gated Apartments</div>
+          <div style={{paddingTop: '8%', fontSize: '32px', fontWeight: '500',color:"#D6AD60",fontFamily:"Lora"}}>Living spaces in Sainikpuri, <br /> Hyderabad</div>
+          <div style={{paddingTop: '8%', paddingBottom: '8%', fontSize: '32px', fontWeight: '500',color:"#D6AD60",fontFamily:"Lora"}}>Ready to Move-In</div>
+          <div className='download_broch'>Download Brochure</div>
+        </div>
+        <div style={{width: '60%'}}>
+          <img src="https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg" style={{width: '60vw', height: '70vh'}}/>
+        </div>
+      </div> 
+    </div>
+                </div>
+              
                 {/* 2nd part */}
                 <div className="background-div">
                     <div className='frst_box'>
@@ -123,7 +169,100 @@ const AbodeMainLandingPage = () => {
                     <div className='download_broch'>Download Brochure</div>
                 </div>
                 {/* 4th part */}
-                <div></div>
+                <div className='fourth_part'>
+                <div className='comn_hox22'>
+                        <div className='yellow_head2'>Specifications</div>
+                </div>
+                {/* bydefault */}
+                {specification == "livingRoom" &&
+                <div className='overALL_Img'>
+                    <div><img className='immm1' src={changeImage1}/></div>
+                    <div>
+                        <div>
+                            <div className='third_frst'>Living/Dining Room</div>
+                            <div className='gapppppguys'>
+                                <span><img style={{ height: "20px", width: "20px" }} alt='' src={star} /></span>
+                                <span className='walls'>Floor - Marble</span>
+                            </div>
+                            <div className='gapppppguys'>
+                                <span><img style={{ height: "20px", width: "20px" }} alt='' src={star} /></span>
+                                <span className='walls'>Walls - Acrylic Emulsion / OBD</span>
+                            </div>
+                            <div className='gapppppguys'>
+                            <span><img style={{ height: "20px", width: "20px" }} alt='' src={star} /></span>
+                                <span className='walls'>Celling - Acrylic Emulsion / OBD</span>
+                            </div>
+                        </div>
+                        <div className='gapagain'>
+                            <div><img src={left_grey}/></div>
+                            <div className='walls's>1/3</div>
+                            <div onClick={()=>{setSpecification("Bedroom")}}><img src={right_arr}/></div>
+                        </div>
+                    </div>
+                </div>}
+                {/* thenNext */}
+                {specification == "Bedroom" &&                
+                <div className='overALL_Img'>
+                    <div><img className='immm1' src={changeImage2}/></div>
+                    <div>
+                        <div>
+                            <div className='third_frst'>Bedroom</div>
+                            <div className='gapppppguys'>
+                                <span><img style={{ height: "20px", width: "20px" }} alt='' src={star} /></span>
+                                <span className='walls'>Floor - Laminated wooden flooring</span>
+                            </div>
+                            <div className='gapppppguys'>
+                                <span><img style={{ height: "20px", width: "20px" }} alt='' src={star} /></span>
+                                <span className='walls'>Walls - Acrylic Emulsion / OBD</span>
+                            </div>
+                            <div className='gapppppguys'>
+                            <span><img style={{ height: "20px", width: "20px" }} alt='' src={star} /></span>
+                                <span className='walls'>Celling - Acrylic Emulsion / OBD</span>
+                            </div>
+                        </div>
+                        <div className='gapagain'>
+                            <div onClick={()=>{setSpecification("livingRoom")}}><img src={left_arr}/></div>
+                            <div className='walls's>2/3</div>
+                            <div onClick={()=>{setSpecification("Kitchen")}}><img src={right_arr}/></div>
+                        </div>
+                    </div>
+                </div>}
+                {/* againNext */}
+                {specification == "Kitchen" &&                
+                <div className='overALL_Img'>
+                    <div><img className='immm1' src={changeImage3}/></div>
+                    <div>
+                        <div>
+                            <div className='third_frst'>Kitchen</div>
+                            <div className='gapppppguys'>
+                                <span><img style={{ height: "20px", width: "20px" }} alt='' src={star} /></span>
+                                <span className='walls'>Walls- Tiles up-to 2’ above counter & acrylic emulsion paint in balance area</span>
+                            </div>
+                            <div className='gapppppguys'>
+                                <span><img style={{ height: "20px", width: "20px" }} alt='' src={star} /></span>
+                                <span className='walls'>Floor - Anti-skid Tiles</span>
+                            </div>
+                            <div className='gapppppguys'>
+                            <span><img style={{ height: "20px", width: "20px" }} alt='' src={star} /></span>
+                                <span className='walls'>Celling - OBD</span>
+                            </div>
+                            <div className='gapppppguys'>
+                            <span><img style={{ height: "20px", width: "20px" }} alt='' src={star} /></span>
+                                <span className='walls'>Counter - Granite /Synthetic Stone</span>
+                            </div>
+                            <div className='gapppppguys'>
+                            <span><img style={{ height: "20px", width: "20px" }} alt='' src={star} /></span>
+                                <span className='walls'>Fittings / Fixtures - CP fittings, SS Sink, Exhaust fan</span>
+                            </div>
+                        </div>
+                        <div className='gapagain'>
+                            <div onClick={()=>{setSpecification("Bedroom")}}><img src={left_arr}/></div>
+                            <div className='walls's>3/3</div>
+                            <div><img src={right_grey}/></div>
+                        </div>
+                    </div>
+                </div>}
+                </div>
                 {/* 5th part */}
                 <div style={{backgroundColor:"#122620",textAlign:"center"}}>
                     <div className='gallery'>Gallery</div>
