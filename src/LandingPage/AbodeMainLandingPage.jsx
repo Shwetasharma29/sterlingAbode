2a89cd55df40a4518adfab755cc49d29b86b154b
shweta sharma <shweta29sharma06@gmail.com>
2024-03-02 21:35:01 +0530

    update

--- src/LandingPage/AbodeMainLandingPage.jsx
+++ src/LandingPage/AbodeMainLandingPage.jsx
@@ -1,8 +1,9 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import "./abode.css"
 import map from "../Assets/map.svg"
 import star from "../Assets/star.svg"
-import { Carousel } from '3d-react-carousal';
+// import { Carousel } from '3d-react-carousal';
+import Carousel from 'react-spring-3d-carousel';
 import logoo from "../Assets/logoo.svg"
 import left_grey from "../Assets/left_grey.svg"
 import right_arr from "../Assets/right_arr.svg"
@@ -44,18 +45,61 @@ import floor8 from '../Assets/floor4.svg'
 
 const AbodeMainLandingPage = () => {
     const [activeTab, setActiveTab] = useState("NearBy")
-    let slides = [
-        <img style={{
-            width: "48vw", height: "74vh"
-        }} src={mainImage} alt="1" />,
-        <img style={{
-            width: "48vw", height: "74vh"
-        }} src={mainImage2} alt="2" />,
-        <img style={{
-            width: "48vw", height: "74vh"
-        }} src={mainImage3} alt="5" />];
+    const [classS, setClassS] = useState(['preactive', 'active', 'proactive'])
+    const [bhkk, setBhkk] = useState()
+
+    const slides = [
+        {
+            key: 1,
+            content: <img 
+            // style={{
+            //     width: "852px", height: "469px"
+            // }}
+             src={mainImage} alt="1" />
+        },
+        {
+            key: 2,
+            content: <img
+            //  style={{
+            //     width: "852px", height: "469px"
+            // }} 
+            src={mainImage2} alt="2" />
+        },
+        {
+            key: 3,
+            content: <img
+            //  style={{
+            //     width: "852px", height: "469px"
+            // }} 
+            src={mainImage3} alt="5" />
+        },
+        
+    ];
+    // const slides = [
+    //     <img style={{
+    //         width: "852px", height: "469px"
+    //     }} src={mainImage} alt="1" />,
+    //     <img style={{
+    //         width: "852px", height: "469px"
+    //     }} src={mainImage2} alt="2" />,
+    //     <img style={{
+    //         width: "852px", height: "469px"
+    //     }} src={mainImage3} alt="5" />];
+
+    const getChildern = () => {
+        const children = document.getElementsByClassName("css-1qzevvg")
+        children[0].childNodes[0].src = left_arr
+        children[0].childNodes[0].id = "blahblah"
+        children[0].childNodes[1].src = right_arr
+        children[0].childNodes[1].id = "blahblah1"
+    }
+
+    useEffect(() => {
+        getChildern()
+    }, [])
+
     const [specification, setSpecification] = useState("livingRoom");
-    const [bhkk,setBhkk] = useState(false);
+
     const [count, setCount] = useState(0)
 
     const handleNext = () => {
@@ -65,7 +109,7 @@ const AbodeMainLandingPage = () => {
     const handlePrev = () => {
         setCount(count - 1)
     }
-    
+
     return (
         <div>
             <div>
@@ -84,7 +128,7 @@ const AbodeMainLandingPage = () => {
                                 <span className='first_heading'>CONTACT</span>
                             </div>
                             <div className='for_gaaap'>
-                                <span  class="your-image-class">
+                                <span>
                                     <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="20" height="20" viewBox="0 0 48 48">
                                         <path fill="#fff" d="M4.868,43.303l2.694-9.835C5.9,30.59,5.026,27.324,5.027,23.979C5.032,13.514,13.548,5,24.014,5c5.079,0.002,9.845,1.979,13.43,5.566c3.584,3.588,5.558,8.356,5.556,13.428c-0.004,10.465-8.522,18.98-18.986,18.98c-0.001,0,0,0,0,0h-0.008c-3.177-0.001-6.3-0.798-9.073-2.311L4.868,43.303z"></path><path fill="#fff" d="M4.868,43.803c-0.132,0-0.26-0.052-0.355-0.148c-0.125-0.127-0.174-0.312-0.127-0.483l2.639-9.636c-1.636-2.906-2.499-6.206-2.497-9.556C4.532,13.238,13.273,4.5,24.014,4.5c5.21,0.002,10.105,2.031,13.784,5.713c3.679,3.683,5.704,8.577,5.702,13.781c-0.004,10.741-8.746,19.48-19.486,19.48c-3.189-0.001-6.344-0.788-9.144-2.277l-9.875,2.589C4.953,43.798,4.911,43.803,4.868,43.803z"></path><path fill="#cfd8dc" d="M24.014,5c5.079,0.002,9.845,1.979,13.43,5.566c3.584,3.588,5.558,8.356,5.556,13.428c-0.004,10.465-8.522,18.98-18.986,18.98h-0.008c-3.177-0.001-6.3-0.798-9.073-2.311L4.868,43.303l2.694-9.835C5.9,30.59,5.026,27.324,5.027,23.979C5.032,13.514,13.548,5,24.014,5 M24.014,42.974C24.014,42.974,24.014,42.974,24.014,42.974C24.014,42.974,24.014,42.974,24.014,42.974 M24.014,42.974C24.014,42.974,24.014,42.974,24.014,42.974C24.014,42.974,24.014,42.974,24.014,42.974 M24.014,4C24.014,4,24.014,4,24.014,4C12.998,4,4.032,12.962,4.027,23.979c-0.001,3.367,0.849,6.685,2.461,9.622l-2.585,9.439c-0.094,0.345,0.002,0.713,0.254,0.967c0.19,0.192,0.447,0.297,0.711,0.297c0.085,0,0.17-0.011,0.254-0.033l9.687-2.54c2.828,1.468,5.998,2.243,9.197,2.244c11.024,0,19.99-8.963,19.995-19.98c0.002-5.339-2.075-10.359-5.848-14.135C34.378,6.083,29.357,4.002,24.014,4L24.014,4z"></path><path fill="#40c351" d="M35.176,12.832c-2.98-2.982-6.941-4.625-11.157-4.626c-8.704,0-15.783,7.076-15.787,15.774c-0.001,2.981,0.833,5.883,2.413,8.396l0.376,0.597l-1.595,5.821l5.973-1.566l0.577,0.342c2.422,1.438,5.2,2.198,8.032,2.199h0.006c8.698,0,15.777-7.077,15.78-15.776C39.795,19.778,38.156,15.814,35.176,12.832z"></path><path fill="#fff" fill-rule="evenodd" d="M19.268,16.045c-0.355-0.79-0.729-0.806-1.068-0.82c-0.277-0.012-0.593-0.011-0.909-0.011c-0.316,0-0.83,0.119-1.265,0.594c-0.435,0.475-1.661,1.622-1.661,3.956c0,2.334,1.7,4.59,1.937,4.906c0.237,0.316,3.282,5.259,8.104,7.161c4.007,1.58,4.823,1.266,5.693,1.187c0.87-0.079,2.807-1.147,3.202-2.255c0.395-1.108,0.395-2.057,0.277-2.255c-0.119-0.198-0.435-0.316-0.909-0.554s-2.807-1.385-3.242-1.543c-0.435-0.158-0.751-0.237-1.068,0.238c-0.316,0.474-1.225,1.543-1.502,1.859c-0.277,0.317-0.554,0.357-1.028,0.119c-0.474-0.238-2.002-0.738-3.815-2.354c-1.41-1.257-2.362-2.81-2.639-3.285c-0.277-0.474-0.03-0.731,0.208-0.968c0.213-0.213,0.474-0.554,0.712-0.831c0.237-0.277,0.316-0.475,0.474-0.791c0.158-0.317,0.079-0.594-0.04-0.831C20.612,19.329,19.69,16.983,19.268,16.045z" clip-rule="evenodd"></path>
                                     </svg>
@@ -93,25 +137,27 @@ const AbodeMainLandingPage = () => {
                             </div>
                         </div>
                         <div style={{ display: 'flex' }}>
-                            <div style={{ width: '40%', paddingTop: '3%', paddingLeft: '5%' }}>
-                                {/* <img src={txt} />*/}
-                                <div className='main_bord'>
-            <div className='sterling' style={{fontFamily:"Cormorant Garamond"}}>
-              STERLING
-            </div>
-            <div className='sterling' style={{fontFamily:"Cormorant Garamond"}}>ABODE</div>
-            <div className='sterling22' style={{fontFamily:"Cormorant Garamond"}}>Redifining luxury</div>
-          </div> 
-                                <div className='lux' style={{fontFamily: "Lora" }}>Luxury Gated Apartments</div>
-                                <div className='livingss' style={{  fontFamily: "Lora" }}>Living spaces in Sainikpuri, <br /> Hyderabad</div>
-                                <div className='readdy' style={{ fontFamily: "Lora" }}>Ready to Move-In</div>
+                            <div style={{ width: '40%', paddingTop: '3.8%', paddingLeft: '5%' }}>
+                            <div className='main_bord'>
+                              <div className='sterlinggg' style={{fontFamily:"Cormorant Garamond"}}>
+                                STERLING
+                              </div>
+                              <div  className='sterlinggg' style={{fontFamily:"Cormorant Garamond"}}>ABODE</div>
+                              <div  className='lux' style={{fontFamily:"Cormorant Garamond"}}>Redifining luxury</div>
+                            </div>
+                                <div className='lux' style={{ fontFamily: "Lora" }}>Luxury Gated Apartments</div>
+                                <div className='livingss' style={{fontFamily: "Lora" }}>Living spaces in Sainikpuri, <br /> Hyderabad</div>
+                                <div className='ready' style={{ fontFamily: "Lora" }}>Ready to Move-In</div>
                                 <div className='download_broch'>Download Brochure</div>
                             </div>
-                            <div style={{ width: '65%' }} >
-                                {count % 3 === 0  && <img src={t1} style={{ width: '60vw', height: '82vh' }} />}
-                                {count % 3 === 1  && <img src={t2} style={{ width: '60vw', height: '82vh' }} />}
-                                {count % 3 === 2  && <img src={t3} style={{ width: '60vw', height: '82vh' }} />}
+                            <div style={{ width: '65%', position: 'relative', top: '-6.6rem', zIndex: 1 }} >
+                                {count % 3 === 0  && <img src={t1} style={{ width: '60vw', height: '100vh' }} />}
+                                {count % 3 === 1  && <img src={t2} style={{ width: '60vw', height: '100vh' }} />}
+                                {count % 3 === 2  && <img src={t3} style={{ width: '60vw', height: '100vh' }} />}
                             </div>
+                            {count % 3 === 0  && <span className='imageText'>Lush Green Surroundings</span>}
+                            {count % 3 === 1  && <span className='imageText'>Lush Green Surroundings</span>}
+                            {count % 3 === 2  && <span className='imageText'>Lush Green Surroundings</span>}
                             <div className="useBtns">
                                 <img src={left_arr} onClick={handlePrev} style={{marginRight: '1rem', width: '2.3rem'}}/>
                                 <img src={right_arr} onClick={handleNext} style={{width: '2.3rem'}}/>
@@ -151,52 +197,52 @@ const AbodeMainLandingPage = () => {
                     <div className='Modern_spaced'>Modern Spaced Designed for You</div>
                     <div className='ssssss'>
                         <div class="grid-container">
-                            <div class="grid-item" style={{ borderRight: "1px solid #D4AF37", borderBottom: "1px solid #D4AF37" }}>
-                                <div><img src={img1} /></div>
+                            <div class="grid-item grid_1">
+                                <div><img className='gridd_hai_guys' src={img1} /></div>
                                 <div className='amen_head'>Jogging Track</div>
                             </div>
-                            <div class="grid-item" style={{ borderRight: "1px solid #D4AF37", borderBottom: "1px solid #D4AF37" }}>
-                                <div><img src={img2} /></div>
+                            <div class="grid-item grid_2">
+                                <div><img className='gridd_hai_guys' src={img2} /></div>
                                 <div className='amen_head'>Swimming Pool</div>
                             </div>
-                            <div class="grid-item" style={{ borderRight: "1px solid #D4AF37", borderBottom: "1px solid #D4AF37" }}>
-                                <div><img src={img3} /></div>
+                            <div class="grid-item grid_3">
+                                <div><img className='gridd_hai_guys' src={img3} /></div>
                                 <div className='amen_head'>24x7 Security</div>
                             </div>
-                            <div class="grid-item" style={{ borderBottom: "1px solid #D4AF37" }}>
-                                <div><img src={img4} /></div>
+                            <div class="grid-item grid_4" >
+                                <div><img className='gridd_hai_guys' src={img4} /></div>
                                 <div className='amen_head'>Gymnasium</div>
                             </div>
-                            <div class="grid-item" style={{ borderRight: "1px solid #D4AF37", borderBottom: "1px solid #D4AF37" }}>
-                                <div><img src={img5} /></div>
+                            <div class="grid-item grid_5" >
+                                <div><img className='gridd_hai_guys' src={img5} /></div>
                                 <div className='amen_head'>Water Softener Plant </div>
                             </div>
-                            <div class="grid-item" style={{ borderRight: "1px solid #D4AF37", borderBottom: "1px solid #D4AF37" }}>
-                                <div><img src={img6} /></div>
+                            <div class="grid-item grid_6" >
+                                <div><img className='gridd_hai_guys' src={img6} /></div>
                                 <div className='amen_head'>Yoga/Meditation Area</div>
                             </div>
-                            <div class="grid-item" style={{ borderRight: "1px solid #D4AF37", borderBottom: "1px solid #D4AF37" }}>
-                                <div><img src={img7} /></div>
+                            <div class="grid-item grid_7">
+                                <div><img  className='gridd_hai_guys'src={img7} /></div>
                                 <div className='amen_head'>Senior Citizen Site Out</div>
                             </div>
-                            <div class="grid-item" style={{ borderBottom: "1px solid #D4AF37" }}>
-                                <div><img src={img8} /></div>
+                            <div class="grid-item grid_8">
+                                <div><img className='gridd_hai_guys' src={img8} /></div>
                                 <div className='amen_head'>Lawn Tennis Court</div>
                             </div>
-                            <div class="grid-item" style={{ borderRight: "1px solid #D4AF37" }}>
-                                <div><img src={img9} /></div>
+                            <div class="grid-item grid_9">
+                                <div><img  className='gridd_hai_guys'src={img9} /></div>
                                 <div className='amen_head'>Party Lawn</div>
                             </div>
-                            <div class="grid-item" style={{ borderRight: "1px solid #D4AF37" }}>
-                                <div><img src={img10} /></div>
+                            <div class="grid-item grid_10">
+                                <div><img className='gridd_hai_guys' src={img10} /></div>
                                 <div className='amen_head'>Club House</div>
                             </div>
-                            <div class="grid-item" style={{ borderRight: "1px solid #D4AF37" }}>
-                                <div><img src={img11} /></div>
+                            <div class="grid-item grid_11" >
+                                <div><img className='gridd_hai_guys' src={img11} /></div>
                                 <div className='amen_head'>Children’s Play Area</div>
                             </div>
-                            <div class="grid-item" >
-                                <div><img src={img12} /></div>
+                            <div class="grid-item grid_12" >
+                                <div><img className='gridd_hai_guys' src={img12} /></div>
                                 <div className='amen_head'>Fire Fighting System</div>
                             </div>
                         </div>
@@ -299,14 +345,13 @@ const AbodeMainLandingPage = () => {
                         </div>}
                 </div>
                 {/* 5th part */}
-                <div style={{ backgroundColor: "#122620", textAlign: "center" }}>
+                <div style={{ backgroundColor: "#122620", textAlign: "center", display: 'flex', alignItems: 'center',  flexDirection: 'column' }}>
                     <div className='gallery'>Gallery</div>
                     {/* <div> */}
-                    <Carousel slides={slides} autoplay={false} interval={1000} style={{height:"79vh"}}/>
+                    <Carousel slides={slides} goToSlide={1} showNavigation={true} enableSwipe={true}/>
                     {/* </div> */}
                     <span style={{ textAlign: "-webkit-center" }}>
                         <div className='learn'>Click to enlarge</div>
-
                         <div className='download_broch'>Download Brochure</div>
                     </span>
                     <div style={{ color: "#122620", height: "50px" }}></div>
